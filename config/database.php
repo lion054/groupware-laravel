a8f3be2f1c7da1c89fbb8502a9a708e506a85a11
lion054 <lionel.siduna@gmail.com>
2020-08-04 12:21:30 -0400

    Install neo4j-php-client

--- config/database.php
+++ config/database.php
@@ -78,6 +78,14 @@ return [
             'prefix' => '',
         ],
 
+        'neo4j' => [
+            'driver' => 'neo4j',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '7474'),
+            'username' => env('DB_USERNAME', 'neo4j'),
+            'password' => env('DB_PASSWORD', 'secret'),
+        ],
+
     ],
 
     /*
