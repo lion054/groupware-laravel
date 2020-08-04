0d18b8256d1915af8d1820eff3a79e8b241922a0
lion054 <lionel.siduna@gmail.com>
2020-08-04 12:29:54 -0400

    Implement seeder for company/department/user

---
+++ database/seeds/NeoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use GraphAware\Neo4j\Client\ClientBuilder;
+use Illuminate\Database\Seeder;
+
+class NeoSeeder extends Seeder
+{
+    /**
+     * @var Neo4j PHP Client
+     */
+    protected $client = null;
+
+    /**
+     * Initialize the client
+     */
+    public function __construct()
+    {
+        $host = config('database.connections.neo4j.host');
+        $port = config('database.connections.neo4j.port');
+        $username = config('database.connections.neo4j.username');
+        $password = config('database.connections.neo4j.password');
+
+        $this->client = ClientBuilder::create()
+            ->addConnection('default', "http://$username:$password@$host:$port")
+            ->build();
+    }
+}
