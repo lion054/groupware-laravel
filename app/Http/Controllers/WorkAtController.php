de4f47d864354f83cf59f22311a9a34e81ed1cad
lion054 <lionel.siduna@gmail.com>
2020-08-16 12:13:01 -0400

    Remove unnecessary API

--- app/Http/Controllers/WorkAtController.php
+++ app/Http/Controllers/WorkAtController.php
@@ -6,38 +6,6 @@ use Illuminate\Http\Request;
 
 class WorkAtController extends Controller
 {
-    public function index(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'user_uuid' => 'required',
-        ]);
-        $validator->setAttributeNames([
-            'user_uuid' => 'User UUID',
-        ]);
-        if ($validator->fails()) {
-            return [
-                'success' => FALSE,
-                'errors' => $validator->messages(),
-            ];
-        }
-        $user_uuid = $request->query('user_uuid');
-        $search = $request->query('search');
-        $sort_by = $request->query('sort_by');
-        $limit = $request->query('limit');
-
-        $query = [
-            "MATCH (from:User{ uuid: {from_uuid} })-[r:WORK_AT]->(to:Department)",
-            'RETURN r',
-        ];
-        $records = $this->client->run(implode(' ', $query), [
-            'from_uuid' => $user_uuid,
-        ])->getRecords();
-        $result = [];
-        foreach ($records as $record)
-            $result[] = $record->get('r');
-        return $result;
-    }
-
     public function show($uuid)
     {
         return $this->getRelation($uuid);
