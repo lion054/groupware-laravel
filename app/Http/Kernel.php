bc5ff1289abfbb5cfce611d3c584e78aaf949a19
lion054 <lionel.siduna@gmail.com>
2020-08-21 06:38:58 -0400

    Remove default throttle

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            // 'throttle:60,1', // Remove throttle, because /media must be called frequently
             'bindings',
         ],
     ];
